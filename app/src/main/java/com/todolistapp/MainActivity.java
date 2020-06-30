f82b9c506337378cfb7b81e553515189f3118e10
Darren Chan <dchan3875@bths.edu>
2020-06-30 18:54:19 -0400

    test

--- app/src/main/java/com/todolistapp/MainActivity.java
+++ app/src/main/java/com/todolistapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.todolistapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addToDoButton = (Button) findViewById(R.id.AddButton);
+
     }
 }
